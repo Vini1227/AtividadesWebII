efd7c8a0208c9df14ef7a87e622c1b95dbbdf2e7
Vini1227 <cvsn@discente.ifpe.edu.br>
2025-06-06 11:29:24 -0300

    Atividade 04: implementação das factories, seeders e migrations

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,22 +2,21 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Factory as FakerFactory;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
-    public function run(): void
+    public function run()
     {
-        // User::factory(10)->create();
+        // Inicializa o Faker se precisar
+        FakerFactory::create()->unique(true);
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        // Chama os seeders para popular as tabelas
+        $this->call([
+            CategorySeeder::class,
+            AuthorPublisherBookSeeder::class,
+            UserBorrowingSeeder::class, // Novo seeder adicionado aqui
         ]);
     }
 }
